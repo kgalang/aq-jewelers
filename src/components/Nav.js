5382ecd7c429bfaacaa24759743614c09e9ed61c
kgalang <kgalang5@gmail.com>
2019-10-14 22:10:35 -0400

    Create first version of NavBar and ignore prop-type warnings.

---
+++ src/components/Nav.js
@@ -0,0 +1,172 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+import { media } from '../styles/media'
+import { Hamburger } from './Hamburger'
+import { Link } from 'gatsby'
+
+const ROUTES = [
+  {
+    route: '/',
+    text: 'Home',
+    as: Link,
+    get selected() {
+      return this.route === location.pathname
+    },
+  },
+  {
+    route: '/about',
+    text: 'About',
+    as: Link,
+    get selected() {
+      return this.route === location.pathname
+    },
+  },
+]
+
+const NavContainer = styled.div`
+  height: ${props => props.theme.desktopHeaderHeight};
+  background-color: ${props => props.theme.primaryColor};
+  color: ${props => props.theme.white};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 2rem;
+
+  ${media.forSmallOnly`
+    height: ${props => props.theme.mobileHeaderHeight};
+    padding: 0 1rem;
+  `}
+`
+
+const NavOptionsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
+
+const NavLinkStyles = css`
+  color: ${props => props.theme.white} !important;
+  text-decoration: none;
+  margin-right: 1rem;
+  position: relative;
+  cursor: pointer;
+
+  &:last-child {
+    margin-right: 0;
+  }
+
+  /* &:before, */
+  &:after {
+    content: '';
+    position: absolute;
+    bottom: -7px;
+    width: 0px;
+    height: 3px;
+    margin: 5px 0 0;
+    transition: all 0.4s ease-in-out;
+    opacity: 0;
+    left: 0;
+  }
+
+  &:hover {
+    &:after {
+      opacity: 1;
+      background-color: ${props => props.theme.white};
+      width: 100%;
+    }
+  }
+
+  &.selected {
+    &:after {
+      opacity: 1;
+      background-color: ${props => props.theme.white};
+      width: 100%;
+    }
+  }
+
+  ${media.forSmallOnly`
+    color: ${props => props.theme.defaultFontColor} !important;
+    margin-bottom: 0.25rem;
+    padding: .75rem;
+    margin: 0;
+    font-size: ${props => props.theme.defaultFontSize};
+  `}
+`
+
+const RoutesContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  ${media.forSmallOnly`
+    position: absolute;
+    height: calc(100vh - ${({ theme }) => theme.mobileHeaderHeight});
+    top: ${({ theme }) => theme.mobileHeaderHeight};
+    left: 0;
+    background-color: ${({ theme }) => theme.white};
+    width: 100%;
+    display: ${({ open }) => (open ? 'flex' : 'none')};
+    flex-direction: column;
+    padding: 1rem;
+    z-index: 20;
+  `}
+`
+
+const createRouteLink = as => styled(as)`
+  ${NavLinkStyles};
+`
+
+export const NavComponent = ({
+  routes = ROUTES,
+  containerClassName = '',
+  containerStyle = {},
+  navOptionsStyle = {},
+  renderLeftSection = () => null,
+}) => {
+  const [hamburgerOpen, setHamburgerOpen] = React.useState(false)
+  return (
+    <NavContainer className={containerClassName} style={containerStyle}>
+      {renderLeftSection()}
+
+      <NavOptionsContainer style={navOptionsStyle}>
+        <Hamburger
+          open={hamburgerOpen}
+          onClick={() => setHamburgerOpen(!hamburgerOpen)}
+        />
+        <RoutesContainer open={hamburgerOpen}>
+          {routes.map(
+            ({
+              route,
+              text,
+              as = 'a',
+              selected = false,
+              renderLinkContent,
+              onClick = () => null,
+              ...rest
+            }) => {
+              // The controls what the link renders as so it works with Reach, React Router, and normal
+              const RouteLink = createRouteLink(as)
+
+              return (
+                <RouteLink
+                  to={route}
+                  key={route}
+                  className={selected ? 'selected' : ''}
+                  onClick={() => {
+                    setHamburgerOpen(false)
+
+                    onClick({ route, text, ...rest })
+                  }}
+                  {...rest}
+                >
+                  {renderLinkContent ? renderLinkContent() : text}
+                </RouteLink>
+              )
+            },
+          )}
+        </RoutesContainer>
+      </NavOptionsContainer>
+    </NavContainer>
+  )
+}
+
+export const Nav = props => <NavComponent {...props} />
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Nav } from './Nav'
+import styled from 'styled-components'
+
+const StyledNav = styled(Nav)`
+  a {
+    &:after {
+      background-color: ${props => props.theme.secondaryColor} !important;
+    }
+  }
+`
+
+export const NavBar = () => {
+  return <StyledNav />
+}
