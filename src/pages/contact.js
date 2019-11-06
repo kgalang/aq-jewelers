e3aeaa05a322d0aa7b4d294a739fb0f7aa132188
gcastillo <gcastillo@everfi.com>
2019-11-06 00:25:43 -0500

    updating styling for Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,26 +1,11 @@
 import React from 'react'
-import { Layout } from '../components/Layout'
+import { Layout, Container, ContactForm } from '../components'
 
 const ContactPage = () => (
   <Layout>
-    <form
-      name="contact"
-      method="POST"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <input type="hidden" name="bot-field" />
-      <label htmlFor="name">
-        Name: <input type="text" name="name" id="name" />
-      </label>
-      <label htmlFor="email">
-        Your Email: <input type="email" name="email" id="email" />
-      </label>
-      <label htmlFor="message">
-        Message: <textarea name="message" id="message" />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+    <Container>
+      <ContactForm />
+    </Container>
   </Layout>
 )
 
