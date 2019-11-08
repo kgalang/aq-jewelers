833ec5e0bc30952cadde724b843a286a1860f2c6
gcastillo <gcastillo@everfi.com>
2019-11-07 22:28:58 -0500

    repositioning content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import styled, { css } from 'styled-components'
 import { Container } from '../components'
 import { Link } from 'gatsby'
 import InitialsLogo from '../assets/aq-logo.svg'
+import { media } from '../styles'
 
 const ROUTES = [
   {
@@ -35,34 +36,58 @@ const FooterContainer = styled.div`
   }
 `
 
-const FooterRow = styled.div`
+const FooterContent = styled.div`
   display: flex;
-  flex-flow: row wrap;
-  width: 100%;
+  flex-direction: column;
 `
 
-const Aside = styled.div`
-  flex: 1;
+const FooterInfo = styled.div`
+  display: flex;
+  padding-bottom: 2rem;
+  > * {
+    flex: 1;
+  }
 `
 
-const Address = styled(Aside)`
-  flex: 1;
-`
+// const Aside = styled.div`
+//   flex: 1;
+// `
 
-const Nav = styled(Aside)`
-  flex: 1;
+// const Address = styled(Aside)`
+//   flex: 1;
+// `
+
+// const Nav = styled(Aside)`
+//   flex: 1;
+//   text-align: right;
+// `
+
+const Address = styled.div``
+
+const Nav = styled.div`
   text-align: right;
 `
 
 const LogoContainer = styled.div`
-  text-align: center;
-  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const Logo = styled(InitialsLogo)`
   fill: ${props => props.theme.white};
   height: 5rem;
-  margin: -1rem 0 2rem 0;
+
+  ${media.forSmallOnly`
+    height: 2.5rem;
+  `}
+`
+
+const FooterDisclaimer = styled.div`
+  text-align: center;
+  p {
+    margin: 0;
+  }
 `
 
 const NavLinkStyles = css`
@@ -108,53 +133,55 @@ export const Footer = ({ routes = ROUTES }) => (
     render={data => (
       <FooterContainer>
         <Container>
-          <FooterRow>
-            <Address>
-              <p>{data.prismicFooter.data.address_line_1.text}</p>
-              <p>{data.prismicFooter.data.address_line_2.text}</p>
-              <p>
-                <a href={'mailto:' + data.prismicFooter.data.email.text}>
-                  {data.prismicFooter.data.email.text}
-                </a>
-              </p>
-              <p>
-                {/* replace() removing any parentheses or whitespace to create href for phone number */}
-                <a
-                  href={
-                    'tel:+1' +
-                    data.prismicFooter.data.phone_number.text.replace(
-                      /[^+\d]+/g,
-                      '',
-                    )
-                  }
-                >
-                  {data.prismicFooter.data.phone_number.text}
-                </a>
-              </p>
-            </Address>
-            <Nav>
-              {routes.map(
-                ({ route, text, as = 'a', renderLinkContent, ...rest }) => {
-                  // The controls what the link renders as so it works with Reach, React Router, and normal
-                  const RouteLink = createRouteLink(as)
+          <FooterContent>
+            <FooterInfo>
+              <Address>
+                <p>{data.prismicFooter.data.address_line_1.text}</p>
+                <p>{data.prismicFooter.data.address_line_2.text}</p>
+                <p>
+                  <a href={'mailto:' + data.prismicFooter.data.email.text}>
+                    {data.prismicFooter.data.email.text}
+                  </a>
+                </p>
+                <p>
+                  {/* replace() removing any parentheses or whitespace to create href for phone number */}
+                  <a
+                    href={
+                      'tel:+1' +
+                      data.prismicFooter.data.phone_number.text.replace(
+                        /[^+\d]+/g,
+                        '',
+                      )
+                    }
+                  >
+                    {data.prismicFooter.data.phone_number.text}
+                  </a>
+                </p>
+              </Address>
+              <LogoContainer>
+                <Logo></Logo>
+              </LogoContainer>
+              <Nav>
+                {routes.map(
+                  ({ route, text, as = 'a', renderLinkContent, ...rest }) => {
+                    // The controls what the link renders as so it works with Reach, React Router, and normal
+                    const RouteLink = createRouteLink(as)
 
-                  return (
-                    <RouteLinkContainer key={route}>
-                      <RouteLink to={route} {...rest}>
-                        {renderLinkContent ? renderLinkContent() : text}
-                      </RouteLink>
-                    </RouteLinkContainer>
-                  )
-                },
-              )}
-            </Nav>
-          </FooterRow>
-          <FooterRow>
-            <LogoContainer>
-              <Logo></Logo>
+                    return (
+                      <RouteLinkContainer key={route}>
+                        <RouteLink to={route} {...rest}>
+                          {renderLinkContent ? renderLinkContent() : text}
+                        </RouteLink>
+                      </RouteLinkContainer>
+                    )
+                  },
+                )}
+              </Nav>
+            </FooterInfo>
+            <FooterDisclaimer>
               <p>{data.prismicFooter.data.footer_text.text}</p>
-            </LogoContainer>
-          </FooterRow>
+            </FooterDisclaimer>
+          </FooterContent>
         </Container>
       </FooterContainer>
     )}
